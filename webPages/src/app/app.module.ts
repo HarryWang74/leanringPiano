a658054aeb59abd93b58277be981e49fa6643c33
HarryWang74 <harry74323@gmail.com>
2021-07-31 18:41:38 +1000

    update

--- webPages/src/app/app.module.ts
+++ webPages/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { KeyboardComponent } from './keyboard/keyboard.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimelineComponent,
-    KeyboardComponent
+    KeyboardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
